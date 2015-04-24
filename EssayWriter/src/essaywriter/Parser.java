88000e90f5eff477688d2b06edc6a9afaf218730
atmarsh <alonzotmarsh@gmail.com>
2015-04-24 12:30:26 -0500

    Finished parsing class.
    Methods work fairly well. Eliminates duplicates and creates special punctuation words.

--- EssayWriter/src/essaywriter/Parser.java
+++ EssayWriter/src/essaywriter/Parser.java
@@ -6,6 +6,7 @@
 package essaywriter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,21 +14,78 @@ import java.util.ArrayList;
  */
 public class Parser {
     
+    private static char[] punctuationArray = {'.', ',', '!', '?'};
+    private static ArrayList<Word> words; 
+    
     public static ArrayList<Word> parse(String text) {
         text = text.toLowerCase();
         String temp = "";
         Word lastWord = null;
+        words = new ArrayList<>();
         for(int i = text.length() - 1; i >= 0; i--) {
-            if(Character.isLetter(text.charAt(i))) {
-                temp += text.charAt(i);
+            if(Character.isLetterOrDigit(text.charAt(i))) {
+                temp = text.charAt(i) + temp;
             } else if(Character.isSpaceChar(text.charAt(i))) {
-                lastWord = Word.generateNewWord(temp, lastWord);
+                lastWord = updateList(temp, lastWord);
                 temp = "";
-            } else if(Character.isDigit(text.charAt(i))) {
-                
             } else if(Character.isWhitespace(text.charAt(i))) {
-                
+                lastWord = updateList(temp, lastWord);
+                temp = "";
+                lastWord = updateList("PARAGRAPH_BREAK", lastWord);
+                words.add(lastWord);
+            } else if(isPunctuation(text.charAt(i))) {
+                lastWord = updateList(temp, lastWord);
+                lastWord = updateList(text.charAt(i) + "", lastWord);
+                temp = "";
             }
         }
+        lastWord = updateList(temp, lastWord);
+        words.add(lastWord);
+        temp = "";
+        return words;
+    }
+    
+    public static boolean isPunctuation(char ch) {
+        for(char punct : punctuationArray) {
+            if(punct == ch) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static char simplifyPunctuation(char ch) {
+        switch(ch) {
+            case '.': return ch;
+            case ',': return ch;
+            case '?': return '.';
+            case '!': return '.';
+            default : return '.';
+        }
+    }
+    
+    public static Word updateList(String wordString, Word lastWord) {
+        boolean contains = false;
+        Word prevWord = Word.getErrorWord();
+        for(Word word : words) {
+            if(word.getWord().equals(wordString)) {
+                word.addWordAfter(lastWord);
+                contains = true;
+                prevWord = word;
+            }
+        }
+        if(!contains) {
+            prevWord = Word.generateNewWord(wordString, lastWord);
+            words.add(prevWord);
+        }
+        return prevWord;
+    }
+    
+    public static void printWordList(List<Word> words) {
+        System.out.print("[");
+        for(Word word : words) {
+            System.out.print(word.getWord() + ", ");
+        }
+        System.out.println("END]");
     }
 }
