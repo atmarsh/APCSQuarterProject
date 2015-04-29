5d491dc617cfd22db61919a4ee0b8ddb958409cb
atmarsh <alonzotmarsh@gmail.com>
2015-04-29 13:09:49 -0500

    Compatibility Update
    Made the code compatible with other classes. Included a few parameter changes.

--- EssayWriter/src/essaywriter/WritingCenter.java
+++ EssayWriter/src/essaywriter/WritingCenter.java
@@ -2,23 +2,29 @@ package essaywriter;
 
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Student
  */
 public class WritingCenter {
-    Random random = new Random();
-    ArrayList<String> myEssay = new ArrayList<String>();
+    private static Random random = new Random();
+    private static ArrayList<String> myEssay = new ArrayList<String>();
+
+    public static ArrayList<String> getMyEssay() {
+        return myEssay;
+    }
     
-    public Word getNextWord(Word thisWord){
+    public static Word getNextWord(Word thisWord){
         int ourChoice = random.nextInt(thisWord.getFollowingWords().size());
         return thisWord.getFollowingWords().get(ourChoice);
     }
     
-    public void generateEssay(int numberOfWords){
-        int firstWord = random.nextInt(EssayWriter.getWords().size());
-        Word currentWord = EssayWriter.getWords().get(firstWord);
+    public static void generateEssay(List<Word> words){
+        int numberOfWords = words.size();
+        int firstWord = random.nextInt(numberOfWords);
+        Word currentWord = words.get(firstWord);
         myEssay.add(wordAsString(currentWord));
         while(myEssay.size() < numberOfWords){
             int nextWord = random.nextInt(currentWord.getFollowingWords().size());
@@ -27,13 +33,13 @@ public class WritingCenter {
         }
     }
     
-    public String wordAsString(Word word){
+    public static String wordAsString(Word word){
         String output = word.getWord();
-        if(myEssay.get(myEssay.size() - 1) == "\n"){
+        if(word.getWord() == "\n"){
             output = "\n\t.";
         }
         if(myEssay.size() > 0){
-            if(myEssay.get(myEssay.size() - 1) == ". " || myEssay.get(myEssay.size() - 1) == "\n\t. "){
+            if(myEssay.get(myEssay.size() - 1) == "." || myEssay.get(myEssay.size() - 1) == "\n\t."){
                 output = Character.toUpperCase(output.charAt(0)) + output.substring(1);
             }
         }else{
@@ -43,7 +49,7 @@ public class WritingCenter {
         return output;
     }
     
-    public String getEssay(){
+    public static String getEssay(){
         String essay = "";
         for(int i = 0; i < myEssay.size(); i++){
             essay += myEssay.get(i);
