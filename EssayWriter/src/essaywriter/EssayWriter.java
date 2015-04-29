a8517a564976c9ca49bd39c62cda98cea712b0f8
atmarsh <alonzotmarsh@gmail.com>
2015-04-29 13:10:29 -0500

    Reads and Writes Update
    This main method will now read and then write a new paper.

--- EssayWriter/src/essaywriter/EssayWriter.java
+++ EssayWriter/src/essaywriter/EssayWriter.java
@@ -5,21 +5,33 @@
  */
 package essaywriter;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author atmarsh
  */
 public class EssayWriter {
-    private static List<Word> words;
+
     private static String string = "Hi there, this is a really fucking long sentence and I don't like it. And this sentence is really important too.";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Parser.printWordList(Parser.parse(string));
+        try {
+            string = Reader.readFile("src/essaywriter/newDoc.txt");
+        } catch (IOException ex) {
+            Logger.getLogger(EssayWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(string);
+        List<Word> words = Parser.parse(string);
+        Parser.printWordList(words);
+        WritingCenter.generateEssay(words);
+        System.out.println(WritingCenter.getEssay());
     }
     
-    public static List<Word> getWords() {
-        return words;
-    }
 }
