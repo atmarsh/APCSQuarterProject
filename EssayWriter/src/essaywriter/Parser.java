2bed777734e4b404795319e90160e3925ec89d11
atmarsh <alonzotmarsh@gmail.com>
2015-05-05 13:09:56 -0500

    Proper Noun Functionality Added
    Words that are proper nouns will now be printed and remembered as proper nouns.

--- EssayWriter/src/essaywriter/Parser.java
+++ EssayWriter/src/essaywriter/Parser.java
@@ -13,78 +13,127 @@ import java.util.List;
  * @author atmarsh
  */
 public class Parser {
-    
+
     private static char[] punctuationArray = {'.', ',', '!', '?'};
-    private static ArrayList<Word> words; 
-    
+    private static ArrayList<Word> words;
+
     public static ArrayList<Word> parse(String text) {
-        text = text.toLowerCase();
         String temp = "";
         Word lastWord = Word.getErrorWord();
         words = new ArrayList<>();
-        for(int i = text.length() - 1; i >= 0; i--) {
-            if(Character.isLetterOrDigit(text.charAt(i))) {
+        for (int i = text.length() - 1; i >= 0; i--) {
+            if (Character.isLetterOrDigit(text.charAt(i))) {
                 temp = text.charAt(i) + temp;
-            } else if(Character.isSpaceChar(text.charAt(i))) {
-                lastWord = updateList(temp, lastWord);
+            } else if (Character.isSpaceChar(text.charAt(i))) {
+                if (temp != "") {
+                    if (Character.isUpperCase(temp.charAt(0)) && Character.isLetterOrDigit(text.charAt(i - 1))) {
+                        lastWord = updateList(temp.toLowerCase(), lastWord, true);
+                    } else {
+                        lastWord = updateList(temp.toLowerCase(), lastWord, false);
+                    }
+                }
+
                 temp = "";
-            } else if(Character.isWhitespace(text.charAt(i))) {
-                lastWord = updateList(temp, lastWord);
+            } else if (Character.isWhitespace(text.charAt(i))) {
+                if (temp != "") {
+                    if (Character.isUpperCase(text.charAt(i + 1)) && Character.isLetterOrDigit(text.charAt(i - 1))) {
+                        lastWord = updateList(temp.toLowerCase(), lastWord, true);
+                    } else {
+                        lastWord = updateList(temp.toLowerCase(), lastWord, false);
+                    }
+                }
                 temp = "";
                 lastWord = updateList("PARAGRAPH_BREAK", lastWord);
                 words.add(lastWord);
-            } else if(isPunctuation(text.charAt(i))) {
-                lastWord = updateList(temp, lastWord);
-                lastWord = updateList(simplifyPunctuation(text.charAt(i)) + "", lastWord);
+            } else if (isPunctuation(text.charAt(i))) {
+                if (temp != "") {
+                    if (temp != "") {
+                        if (Character.isUpperCase(temp.charAt(0)) && Character.isLetterOrDigit(text.charAt(i - 1))) {
+                            lastWord = updateList(temp.toLowerCase(), lastWord, true);
+                        } else {
+                            lastWord = updateList(temp.toLowerCase(), lastWord, false);
+                        }
+                    }
+                }
+                lastWord = updateList(simplifyPunctuation(text.charAt(i)) + "", lastWord, false);
                 temp = "";
             }
         }
-        lastWord = updateList(temp, lastWord);
+        lastWord = updateList(temp.toLowerCase(), lastWord);
         temp = "";
         return words;
     }
-    
+
     public static boolean isPunctuation(char ch) {
-        for(char punct : punctuationArray) {
-            if(punct == ch) {
+        for (char punct : punctuationArray) {
+            if (punct == ch) {
                 return true;
             }
         }
         return false;
     }
-    
+
     public static char simplifyPunctuation(char ch) {
-        switch(ch) {
-            case '.': return ch;
-            case ',': return ch;
-            case '?': return '.';
-            case '!': return '.';
-            default : return '.';
+        switch (ch) {
+            case '.':
+                return ch;
+            case ',':
+                return ch;
+            case '?':
+                return '.';
+            case '!':
+                return '.';
+            default:
+                return '.';
         }
     }
-    
+
     public static Word updateList(String wordString, Word lastWord) {
         boolean contains = false;
         Word prevWord = Word.getErrorWord();
-        for(Word word : words) {
-            if(word.getWord().equals(wordString)) {
+        for (Word word : words) {
+            if (word.getWord().equals(wordString)) {
                 word.addWordAfter(lastWord);
                 contains = true;
 //                System.out.println(prevWord.getWord() + " = " + word.getWord());
                 prevWord = word;
             }
         }
-        if(!contains) {
+        if (!contains) {
             prevWord = Word.generateNewWord(wordString, lastWord);
             words.add(prevWord);
         }
         return prevWord;
     }
-    
+
+    public static Word updateList(String wordString, Word lastWord, boolean properNoun) {
+        boolean contains = false;
+        Word prevWord = Word.getErrorWord();
+        for (Word word : words) {
+            if (word.getWord().equals(wordString)) {
+                word.setProperNoun(properNoun);
+                word.addWordAfter(lastWord);
+                contains = true;
+//                System.out.println(prevWord.getWord() + " = " + word.getWord());
+                prevWord = word;
+            }
+        }
+        if (!contains) {
+            prevWord = Word.generateNewWord(wordString, lastWord);
+            prevWord.setProperNoun(properNoun);
+            words.add(prevWord);
+        }
+        return prevWord;
+    }
+
     public static void printWordList(List<Word> words) {
         System.out.print("[");
-        for(Word word : words) {
-            System.out.print(word.getWord() + ", ");
+        for (Word word : words) {
+            if(word.isProperNoun()) {
+                System.out.print(Character.toUpperCase(word.getWord().charAt(0)) + word.getWord().substring(1) + ", ");
+            } else {
+                System.out.print(word.getWord() + ", ");
+            }
         }
         System.out.println("END]");
     }
